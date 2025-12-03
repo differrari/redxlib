59c051c6b0c128efe9dd79cd6784cada64ea8d9e
Diego Ferrari <services@diferrari.com>
2025-12-03 00:00:00 +0000

    [ALLOC] free_sized

--- syscalls.c
+++ syscalls.c
@@ -1,12 +1,19 @@
 #include "ui/draw/draw.h"
 #include "keyboard_input.h"
 
+extern void free(void *ptr);
+
 void commit_draw_ctx (draw_ctx *ctx){
 
 }
 
 void request_draw_ctx(draw_ctx *ctx){
 
+}
+
+void free_sized(void* ptr, size_t size)
+{
+    free(ptr);
 }
 
 void halt(int reason){
